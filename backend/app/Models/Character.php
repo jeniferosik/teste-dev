50b55d996dd8dec403aaa2887eb7786bb40a255c
jeniferosik <jenifer.osik@gmail.com>
2026-07-12 05:19:01 -0300

    feat: add originLocation and currentLocation relationships to Character model

--- backend/app/Models/Character.php
+++ backend/app/Models/Character.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use App\Models\Episode;
+use App\Models\Location;
 
 #[Fillable(['api_id', 'name', 'status', 'type', 'species', 'gender', 'origin_location_id', 'current_location_id', 'image'])]
 class Character extends Model
@@ -12,5 +13,15 @@ class Character extends Model
     public function episodes()
     {
         return $this->belongsToMany(Episode::class);
+    }
+
+    public function originLocation()
+    {
+        return $this->belongsTo(Location::class, 'origin_location_id');
+    }
+
+    public function currentLocation()
+    {
+        return $this->belongsTo(Location::class, 'current_location_id');
     }
 }
